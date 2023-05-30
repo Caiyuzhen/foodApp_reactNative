f2d9c6efdc4fd2dd7b91936a2a14e17a9230a8ea
Caiyuzhen <caiyuzhen00@gmail.com>
2023-05-31 00:19:34 +0800

    test: RN Api

--- view/InfoPage/view.js
+++ view/InfoPage/view.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
-import { View, Text, TextInput, Keyboard, ActionSheetIOS, TouchableOpacity, StyleSheet, Platform, Alert, Animated, AppRegistry, AppState, BackHandler, Dimensions, Image } from "react-native";
+import { 
+	ScrollView, 
+	View, 
+	Text, 
+	TextInput,
+	Keyboard, 
+	ActionSheetIOS, 
+	TouchableOpacity, 
+	StyleSheet, 
+	Platform, 
+	Alert, 
+	Animated, 
+	AppRegistry, 
+	AppState, 
+	BackHandler, 
+	Dimensions, 
+	Image, 
+	NetInfo,
+	PixelRatio,
+	Appearance,
+	Share,
+	Button,
+	Vibration
+} from "react-native";
 // import { CameraRoll } from "@react-native-camera-roll/camera-roll";
 import * as MediaLibrary from 'expo-media-library';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import * as Location from 'expo-location';
+import * as Network from 'expo-network';
+import * as Notifications from 'expo-notifications';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 
 
@@ -21,6 +47,9 @@ export default class InfoPage extends Component {
 			imgUri: null,// 初始化存放相册的数据
 
 			takePhotoUri: null, // 初始化存放拍照的数据
+
+			date: new Date(),
+			showPicker: false,
 		}
 	}
 
@@ -302,8 +331,6 @@ export default class InfoPage extends Component {
 					console.log(pos)
 					alert(JSON.stringify(pos))
 				})
-
-
 				// 移除监听位移距离
 				// walkPos.remove()
 			} 
@@ -342,62 +369,223 @@ export default class InfoPage extends Component {
 	}
 
 
+	// 获取当前网络的状态
+	getNetworkStatus = async () => {
+		try {
+			const networkState = await Network.getNetworkStateAsync()
+				console.log(networkState.isConnected) // true or false
+				console.log(networkState.isInternetReachable) // true or false
+				console.log(networkState.type) // 'unknown', 'none', 'cellular', 'wifi', or 'ethernet'
+			} catch (error) {
+				console.log(error)
+		}
+	}
+
+
+	// 获取屏幕像素比值
+	getPixelRatio = () => {
+		const pixelRatio = PixelRatio.get() 
+		console.log(pixelRatio) // 2 、3 、 3.5 倍屏...
+	}
+
+
+	// 发送 IOS 推送，push NotificationIOS
+	sendPushNotification = async () => {
+		const { status: existingStatus } = await Notifications.getPermissionsAsync()
+		let finalStatus = existingStatus;
+		
+		// 如果权限尚未被授予，则请求权限
+		if (existingStatus !== 'granted') {
+			const { status } = await Notifications.requestPermissionsAsync()
+			finalStatus = status;
+		}
+		
+		// 如果权限被授予，获取设备的推送令牌
+		if (finalStatus === 'granted') {
+			const token = (await Notifications.getExpoPushTokenAsync()).data
+			console.log(token); // 将此令牌发送到服务器，以便将消息推送到设备
+			await scheduleLocalNotification(); // 调用scheduleLocalNotification 方法
+		}
+
+
+		async function scheduleLocalNotification() {
+			await Notifications.scheduleNotificationAsync({
+				content: {
+					title: 'My Notification',
+					body: 'This is a local notification!',
+				},
+					trigger: {
+					seconds: 5, // 5秒后触发通知
+				},
+			})
+		}
+	}
+
+
+	// 获取手机是深色还是浅色
+	getColorScheme = () => {
+		const colorScheme = Appearance.getColorScheme()
+		console.log(colorScheme) // light or dark
+		alert(colorScheme)
+	}
+
+
+	// 分享到第三方渠道
+	shareToApp = async () => {
+		try {
+			const result = await Share.share({
+				message: '你好，我是分享的内容',
+				title: '分享的标题',
+				url: 'https://www.baidu.com', //分享的链接
+				// subject：邮件主题（仅适用于邮件分享）
+				// dialogTitle：分享对话框的标题（仅适用于 Android）
+				// excludedActivityTypes：指定不显示的分享应用程序类型列表
+				// tintColor：分享按钮的颜色
+				// failOnCancel：指定在用户取消分享时是否抛出错误，默认为 false
+			})
+			if (result.action === Share.sharedAction) {
+				if (result.activityType) {
+					// shared with activity type of result.activityType
+				} else {
+					// shared
+				}
+			} else if (result.action === Share.dismissedAction) {
+				// dismissed
+			}
+		} catch (error) {
+			alert(error.message)
+		}
+	}
+
+
+	// 打开时间选择器
+	handleDateChange = (event, selectedDate) => {
+		const currentDate = selectedDate || this.state.date;
+		this.setState({ showPicker: Platform.OS === 'ios', date: currentDate });
+	}
+	
+	showDatePicker = () => {
+		this.setState({ showPicker: true });
+	}
+	
+
+	// 线性马达震动
+	handleVibrate = () => {
+		if (Platform.OS === 'android') {
+				// 在 Android 设备上，需要使用震动模式数组来实现持续时间的震动
+				const PATTERN = [0, 9000]; // 震动 9000 毫秒
+				Vibration.vibrate(PATTERN, false);
+			} else {
+				// 在 iOS 设备上，直接传递持续时间参数即可
+				Vibration.vibrate();
+		}
+	}
+
+
 	render() {
+		const { date, showPicker } = this.state
+		const pixelRatio = PixelRatio.get() 
+
 		return (
-			// 👇可以根据后端的唯一 id 来发送请求, 渲染更详细的页面
-			<View style={styles.container}>
-				<Text onPress={ ()=>{this.getClipboardContent()} }>InfoPage - 测试 RN 的各种原生  API </Text>
-				<TouchableOpacity
-					style={styles.button}
-					onPress={ ()=>{this.showActionSheets()} }>
-					<Text>Show Action Sheet</Text>
-				</TouchableOpacity>
+			<ScrollView>
+				{/* 👇可以根据后端的唯一 id 来发送请求, 渲染更详细的页面 */}
+				<View style={styles.container}>
+					<Text onPress={ ()=>{this.getClipboardContent()} }>InfoPage - 测试 RN 的各种原生  API </Text>
+					<TouchableOpacity
+						style={styles.button}
+						onPress={ ()=>{this.showActionSheets()} }>
+						<Text>Show Action Sheet</Text>
+					</TouchableOpacity>
 
-				{/* keyboard 输入框 */}
-				<TextInput
-					style={styles.input}
-					onChangeText={text => this.setState({ text })}
-					value={this.state.text}
-					onSubmitEditing={ Keyboard.dismiss }
-				/>
+					{/* keyboard 输入框 */}
+					<TextInput
+						style={styles.input}
+						onChangeText={text => this.setState({ text })}
+						value={this.state.text}
+						onSubmitEditing={ Keyboard.dismiss }
+					/>
 
-				<Animated.View  // 🚗 动画 API 【第三步】 👈 Animated 后面可以跟 View, Text, Image, ScrollView 等组件 🚗
-					style={[
-						styles.square, 
-						{opacity: this.state.OpacityFadeAnim,  width: this.state.widthAnim,  height: this.state.heightAnim}
-					]} 
-				>
-					<Text style={{color: 'white'}}>渐变方块</Text>
-				</Animated.View>
+					<Animated.View  // 🚗 动画 API 【第三步】 👈 Animated 后面可以跟 View, Text, Image, ScrollView 等组件 🚗
+						style={[
+							styles.square, 
+							{opacity: this.state.OpacityFadeAnim,  width: this.state.widthAnim,  height: this.state.heightAnim}
+						]} 
+					>
+						<Text style={{color: 'white'}}>渐变方块</Text>
+					</Animated.View>
 
-				{/* <TouchableOpacity
-					style={styles.button}
-					onPress={ ()=>{this.getUserPhotos()} }>
-					<Text>读取相册</Text>
-				</TouchableOpacity> */}
+					{/* <TouchableOpacity
+						style={styles.button}
+						onPress={ ()=>{this.getUserPhotos()} }>
+						<Text>读取相册</Text>
+					</TouchableOpacity> */}
 
-				<TouchableOpacity style={styles.button} onPress={this.pickImage}>
-  					<Text>选择照片</Text>
-				</TouchableOpacity>
-
-
-				<TouchableOpacity style={styles.button} onPress={this.getLocation}>
-  					<Text>获取地理位置</Text>
-				</TouchableOpacity>
+					<TouchableOpacity style={styles.button} onPress={this.pickImage}>
+						<Text>选择照片</Text>
+					</TouchableOpacity>
 
 
-				<TouchableOpacity style={styles.button} onPress={this.openCameraFn}>
-  					<Text>开启摄像头</Text>
-				</TouchableOpacity>
+					<TouchableOpacity style={styles.button} onPress={this.getLocation}>
+						<Text>获取地理位置</Text>
+					</TouchableOpacity>
 
 
-				{/* 👇有图就渲染图片 , 记得设置图片的宽高, 🔥 不然显示不出来！！！*/}
-				{this.state.imgUri && <Image style={styles.image} source={{ uri: this.state.imgUri }} />}
-				{!this.state.imgUri && <View><Text>无图片</Text></View>}
+					<TouchableOpacity style={styles.button} onPress={this.openCameraFn}>
+						<Text>开启摄像头</Text>
+					</TouchableOpacity>
 
-				{this.state.takePhotoUri && <Image style={styles.image} source={{ uri: this.state.takePhotoUri }} />}
-				{!this.state.takePhotoUri && <View><Text>无拍摄图片</Text></View>}
-			</View>
+
+					<TouchableOpacity style={styles.button} onPress={this.getNetworkStatus}>
+						<Text>获取当前网络的状态</Text>
+					</TouchableOpacity>
+
+
+					<TouchableOpacity style={[styles.button, { borderBottomWidth: 3 / pixelRatio}]} onPress={this.getPixelRatio}>
+						<Text>获取屏幕像素比值</Text>
+					</TouchableOpacity>
+
+
+					<TouchableOpacity style={styles.button} onPress={this.sendPushNotification}>
+						<Text>发送 IOS 推送消息</Text>
+					</TouchableOpacity>
+
+					
+					<TouchableOpacity style={styles.button} onPress={this.getColorScheme}>
+						<Text>获取手机是深色还是浅色</Text>
+					</TouchableOpacity>
+
+					
+					<TouchableOpacity style={styles.button} onPress={this.shareToApp}>
+						<Text>分享到第三方渠道</Text>
+					</TouchableOpacity>
+
+
+					<TouchableOpacity style={styles.button} onPress={this.showDatePicker}>
+						<Text>打开时间选择器</Text>
+					</TouchableOpacity>
+					
+					{showPicker && (
+						<DateTimePicker
+							value={date}
+							mode="date"
+							is24Hour={true}
+							display="default"
+							onChange={this.handleDateChange}
+						/>
+					)}
+
+
+					<Button title="震动" onPress={this.handleVibrate} />
+
+
+					{/* 👇有图就渲染图片 , 记得设置图片的宽高, 🔥 不然显示不出来！！！*/}
+					{this.state.imgUri && <Image style={styles.image} source={{ uri: this.state.imgUri }} />}
+					{!this.state.imgUri && <View><Text>无图片</Text></View>}
+
+					{this.state.takePhotoUri && <Image style={styles.image} source={{ uri: this.state.takePhotoUri }} />}
+					{!this.state.takePhotoUri && <View><Text>无拍摄图片</Text></View>}
+				</View>
+			</ScrollView>
 		)
 	}
 }
